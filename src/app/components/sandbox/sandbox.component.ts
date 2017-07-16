f18181c8961c7521ab42832b719e6288e4a7e403
janatbek <jsharsheyev@gmail.com>
2017-07-16 17:33:26 -0600

    assigning classes to the element via class binding and ngClass using function

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,13 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    templateUrl: './sandbox.component.html',
+    // templateUrl: './sandbox.component.html',
+    template: `
+    <h1>Hello World</h1>
+    <h4 [class.special]= "isSpecial">This class binding is special</h4>
+    <h4 [ngClass] = "currentClasses">This div is initially special and saveable</h4>
+    `,
     styleUrls:['./sandbox.component.css']
 })
 
 export class SandboxComponent {
-    name:string = 'John Doe';
-    showName:boolean = false;
-    greeting:number = 5;
+    isSpecial = true;
+    canSave = true;
+    currentClasses = {};
+
+    constructor(){
+        this.setCurrentClasses();
+    }
+
+    setCurrentClasses(){
+        this.currentClasses = {
+            saveable: this.canSave,
+            special:this.isSpecial
+        }
+    }
 }
 
