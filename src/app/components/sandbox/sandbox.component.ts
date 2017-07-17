448d7c40975b2c91c1e127200fe58cd6df39f871
janatbek <jsharsheyev@gmail.com>
2017-07-17 00:43:37 -0600

    Mouse event, note:drag and drag over events didn't work

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -5,11 +5,26 @@ import { Title } from '@angular/platform-browser'
     selector:'app-sandbox',
     template: `
         <h1>Hello Janatbek</h1>
+        <button (click) = "fireEvent($event)">Click Event</button>
+        <br>
+        <button (mouseover) = "fireEvent($event)">Mouseover Event</button>
+        <button (mousedown) = "fireEvent($event)">Mousedown Event</button>
+        <button (dblclick) = "fireEvent($event)">Double Click Event</button>
+        <button (drag) = "fireEvent($event)">Drag Event</button>
+        <button (dragover) = "fireEvent($event)">Drag Over Event</button>
     `
 })
 
 export class SandboxComponent{
     title:string = 'Master Branch';
+    fireEvent(e){
+        // console.log('Button Clicked');
+        // console.log(greeting);
+        // console.log(e);//MouseEvent object        
+        //console.log(e.target);//<button>Click Event</button>
+        //console.log(e.target.id);//the id name if it has an ID
+        console.log(e.type); //the name of the event
+    }
     public constructor(private titleService: Title) {
         this.setTitle();
      }
