867075b2f327bcfdd4a499491be8707465190a44
janatbek <jsharsheyev@gmail.com>
2017-07-16 18:53:52 -0600

    in this branch I used styling elements with ngStyle property and style.{css propertires}

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -4,9 +4,25 @@ import { Component } from '@angular/core';
     selector:'app-sandbox',
     template: `
         <h1>Hello Janatbek</h1>
+        <div [style.font-size] = "isSpecial ? 'x-large' : 'smaller' ">Font size depends on isSpecial</div>
+
+        <div [ngStyle] = "currentStyles">This div initialy saveable and special</div>
     `
 })
 
 export class SandboxComponent{
+    isSpecial = true;
+    canSave = true;
+    currentStyles = {};
 
+    constructor(){
+        this.setCurrentStyles()
+    }
+
+    setCurrentStyles(){
+        this.currentStyles = {
+            'font-style': this.canSave ? 'italic' : 'normal',
+            'font-size' : this.isSpecial ? '24px' : '12px' 
+        }
+    }
 }
