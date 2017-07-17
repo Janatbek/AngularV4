3d6bb9cf6156befedb321ae3d6354fce0dde1229
janatbek <jsharsheyev@gmail.com>
2017-07-17 00:56:19 -0600

    toggle div with boolean and a function that changes a value of a boolean variable

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,17 +2,26 @@ import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser'
 
 @Component({
-    selector:'app-sandbox',
+    selector: 'app-sandbox',
     template: `
         <h1>Hello Janatbek</h1>
+        <button (click) = "toggleValue()">Toogle a div</button>
+        <div *ngIf = 'value'>
+            <h1>Hello World</h1>
+        </div>
     `
 })
 
-export class SandboxComponent{
-    title:string = 'Master Branch';
+export class SandboxComponent {
+    title: string = 'Changing Properties With Events';
+    value: boolean = true;
     public constructor(private titleService: Title) {
         this.setTitle();
-     }
+    }
+
+    toggleValue(){
+        this.value = !this.value;
+    }
 
     public setTitle() {
         this.titleService.setTitle(this.title);
