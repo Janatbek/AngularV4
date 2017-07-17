8f3e887ea570ed4a7173c788f2ebaabf499188d8
janatbek <jsharsheyev@gmail.com>
2017-07-17 00:20:02 -0600

    added title changer function and imported Title dependency from '@angular/platform-browser'

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser'
 
 @Component({
     selector:'app-sandbox',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
 })
 
 export class SandboxComponent{
+    title:string = 'Master Branch';
+    public constructor(private titleService: Title) {
+        this.setTitle();
+     }
 
+    public setTitle() {
+        this.titleService.setTitle(this.title);
+    }
 }
