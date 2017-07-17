1bdab2b76947667d3b687d7f16aa8a673e141734
janatbek <jsharsheyev@gmail.com>
2017-07-17 01:31:36 -0600

    worked with keyup, keydown, focus, blur, cut, copy and paste events and one way data binding by changing the text

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -5,16 +5,52 @@ import { Title } from '@angular/platform-browser'
     selector:'app-sandbox',
     template: `
         <h1>Hello Janatbek</h1>
+        <div>
+            <input type="text" (keyup)="fireEvent($event)" placeholder="keyup event">
+        </div>
+        <div>
+            <input type="text" (keydown)="fireEvent($event)" placeholder="keydown event">
+        </div>
+        <div>
+            <input type="text" (focus)="fireEvent($event)" placeholder="Focus event">
+        </div>
+        <div>
+            <input type="text" (blur)="fireEvent($event)" placeholder="Blur event">
+        </div>
+        <div>
+            <input type="text" (cut)="fireEvent($event)" placeholder="Cut event">
+        </div>
+        <div>
+            <input type="text" (copy)="fireEvent($event)" placeholder="Copy event">
+        </div>
+        <div>
+            <input type="text" (paste)="fireEvent($event)" placeholder="Paste event">
+        </div>
+        <p (copy)="fireEvent($event)">text to be copied</p>
+        <hr>
+        <div>
+            <input type="text" (keyup)="changeText($event)" placeholder="Change Text">
+        </div>
+        <p>{{ text }}</p>
     `
 })
 
 export class SandboxComponent{
-    title:string = 'Master Branch';
+    title:string = 'Keyboard & Input Events';
+    text:string = 'Text to Be Changed';
+
     public constructor(private titleService: Title) {
         this.setTitle();
      }
 
     public setTitle() {
         this.titleService.setTitle(this.title);
+    }
+
+    fireEvent(e){
+        console.log(e.type);
+    }
+    changeText(e){
+        this.text = e.target.value;
     }
 }
